3829be333e6e78a79f4ddcb1f353f61201f17dab
Viktoria Guliyants <viktoria.guliants@yandex.ru>
2026-06-19 15:40:40 +0300

    lending page new format

--- db/migrate/20260325185511_add_name_and_about_to_users.rb
+++ db/migrate/20260325185511_add_name_and_about_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameAndAboutToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :about, :text
+    add_column :users, :name, :string unless column_exists?(:users, :name)
+    add_column :users, :about, :text unless column_exists?(:users, :about)
   end
 end
